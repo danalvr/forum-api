293ec44c6c79c31e0b3c31827e2cfd9ad9ac02ae
Daniel Alvaro <danielkisaran@gmail.com>
2022-09-05 01:52:56 +0700

    Delete comments validation in Threads.js

--- src/Domains/threads/entities/Thread.js
+++ src/Domains/threads/entities/Thread.js
@@ -9,12 +9,12 @@ class Thread {
     this.username = payload.username;
   }
 
-  _verifyPayload({ id, title, body, date, username, comments }) {
-    if (!id || !title || !body || !date || !username || !comments) {
+  _verifyPayload({ id, title, body, date, username }) {
+    if (!id || !title || !body || !date || !username) {
       throw new Error('THREAD.NOT_CONTAIN_NEEDED_PROPERTY');
     }
 
-    if (typeof id !== 'string' || typeof title !== 'string' || typeof body !== 'string' || typeof date !== 'string' || typeof username !== 'string' || !Array.isArray(comments)) {
+    if (typeof id !== 'string' || typeof title !== 'string' || typeof body !== 'string' || typeof date !== 'string' || typeof username !== 'string') {
       throw new Error('THREAD.NOT_MEET_DATA_TYPE_SPECIFICATION');
     }
   }
